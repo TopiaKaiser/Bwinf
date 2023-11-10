608bc418ee022472883c8ba8fbd61033a526e0c5
TopiaKaiser <95187683+TopiaKaiser@users.noreply.github.com>
2023-11-10 15:25:12 +0100

    Update Junior Aufgabe 2.py

--- Junior Aufgabe 2.py
+++ Junior Aufgabe 2.py
@@ -1,1 +1,17 @@
-print("Hello World")
+# Pfad zum Bild angeben
+f = open("bild1.ppm", encoding = "utf-8")
+magicNumber = f.readline().splitlines()
+size_x, size_y = f.readline().split()
+max = f.readline().splitlines()
+body = f.read().splitlines()
+f.close()
+
+img = []
+row = []
+body = [[int(x) for x in row.split()] for row in body]
+for i in range(len(body)):
+    for j in range(0,len(body[i]),3):
+        row.append(([body[i][j],body[i][j+1], body[i][j+2]]))
+        if(len(row) == int(size_x)):
+            img.append(row)
+            row = []
